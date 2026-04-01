6f0b4db285cd0abfba3ca3bbf2bb9a613e126db2
Robert Rudolph <robertrudolph@Roberts-MacBook-Pro.local>
2026-04-01 13:01:28 +0200

    Add FileFluss cask formula for v0.4.0

---
+++ Casks/filefluss.rb
@@ -0,0 +1,19 @@
+cask "filefluss" do
+  version "0.4.0"
+  sha256 :no_check
+
+  url "https://github.com/rana-gmbh/filefluss/releases/download/v#{version}/FileFluss-#{version}.dmg"
+  name "FileFluss"
+  desc "Dual-panel file manager with multi-cloud support"
+  homepage "https://github.com/rana-gmbh/filefluss"
+
+  depends_on macos: ">= :sonoma"
+
+  app "FileFluss.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.rana-gmbh.FileFluss.plist",
+    "~/Library/Caches/com.rana-gmbh.FileFluss",
+    "~/Library/Application Support/com.rana-gmbh.FileFluss",
+  ]
+end
