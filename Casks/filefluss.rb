59ec278d18bf1496ab383b7f17042e1ab79b4a78
Robert Rudolph <robert.rudolph@mailbox.org>
2026-04-11 11:54:55 +0200

    Set sha256 for 0.8.1

--- Casks/filefluss.rb
+++ Casks/filefluss.rb
@@ -1,6 +1,6 @@
 cask "filefluss" do
   version "0.8.1"
-  sha256 :no_check
+  sha256 "29bc5a554f0ef0bb7c6236a4309d46312d6aea0b8074a4a6e6849e95743ac39e"
 
   url "https://github.com/rana-gmbh/filefluss/releases/download/v#{version}/FileFluss-v#{version}.dmg"
   name "FileFluss"
